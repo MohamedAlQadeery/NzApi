3ca2588eb0ba36877329994c86e95ee98f4850ef
MohamedAlQadeery <moh9amad1@gmail.com>
2022-06-26 05:23:58 +0300

    Installing EF Core/Created DbContext And Seeding data

---
+++ NzApp/NzApp.Dal/DbContexts/NzAppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NzApp.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NzApp.Dal.DbContexts
+{
+    public class NzAppDbContext : DbContext
+    {
+        public NzAppDbContext(DbContextOptions<NzAppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+    }
+}
--- NzApp/NzApp.Domain/Models/Walk.cs
+++ NzApp/NzApp.Domain/Models/Walk.cs
@@ -8,10 +8,12 @@ namespace NzApp.Domain.Models
 {
     public class Walk
     {
+        
         public Guid Id { get; set; }
         public string Name { get; set; }
         public double Length { get; set; }
 
+      
         public Guid RegionId { get; set; }
         public Guid WalkDifficultyId { get; set; }
 
